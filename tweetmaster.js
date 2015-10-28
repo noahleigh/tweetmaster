12a5f999d468ef687ba0a05a2f2b47800abb7bd4
ndleigh <ndleigh@iupui.edu>
2015-10-27 20:15:03 -0400

    Basic Tweeting Functionality
    Users can write tweets and see a feed of them on their page. Users can
    only see their tweets. Removed insecure 'autopublish' package.
    Prettified code.

--- tweetmaster.js
+++ tweetmaster.js
@@ -1,8 +1,23 @@
+// Define the collection for holding all tweets if it doesn't exist yet.
+Tweets = new Mongo.Collection('tweets');
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        // code to run on server at startup
+        // Make the logged-in user's tweets available to the client
+        Meteor.publish("tweets", function() {
+            return Tweets.find({
+                authorID: this.userId
+            });
+        });
+    });
+}
+
+
 if (Meteor.isClient) {
 
-    Accounts.ui.config({
-        passwordSignupFields: "USERNAME_AND_EMAIL"
-    });
+    // Look at the published collection of tweets, update if there are changes
+    Meteor.subscribe("tweets");
 
     // Code adapted from http://blog.benmcmahen.com/post/41741539120/building-a-customized-accounts-ui-for-meteor
     Template.signup.events({
@@ -37,58 +52,103 @@ if (Meteor.isClient) {
 
     // Code adapted from http://blog.benmcmahen.com/post/41741539120/building-a-customized-accounts-ui-for-meteor
     Template.login.events({
-    'submit #login' : function(e, t){
-      e.preventDefault();
-      // retrieve the input field values
-      var email = t.find('#login-email').value
-        , password = t.find('#login-password').value;
+        'submit #login': function(e, t) {
+            e.preventDefault();
+            // retrieve the input field values
+            var email = t.find('#login-email').value,
+                password = t.find('#login-password').value;
 
-        // Trim and validate your fields here.... 
+            // Trim and validate your fields here.... 
 
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(email, password, function(err){
-        if (err){
-          // Inform the user that their login attempt has failed. 
-          alert("Wrong email/password");
-          document.getElementById("login-password").value = "";
-        } else{
-          // The user has been logged in.
+            // If validation passes, supply the appropriate fields to the
+            // Meteor.loginWithPassword() function.
+            Meteor.loginWithPassword(email, password, function(err) {
+                if (err) {
+                    // Inform the user that their login attempt has failed. 
+                    alert("Wrong email/password");
+                    document.getElementById("login-password").value = "";
+                } else {
+                    // The user has been logged in.
+                }
+            });
+            return false;
         }
-      });
-         return false; 
-      }
     });
 
     // Handles our logout
     Template.logout.events({
-      'submit #logout': function () {
-        Meteor.logout(function(err){
-          if(err){
-            // Couldn't logout
-            alert("You have not been logged out.");
-          } else {
-            // Tell the user we logged them out
-            alert("You have been logged out.");
-          }
-        });
-        return false;
-      }
+        'submit #logout': function() {
+            Meteor.logout(function(err) {
+                if (err) {
+                    // Couldn't logout
+                    alert("You have not been logged out.");
+                } else {
+                    // Tell the user we logged them out
+                    alert("You have been logged out.");
+                }
+            });
+            return false;
+        }
     });
 
     // Handles our userinfo template
     Template.userinfo.helpers({
-      email: function() {
-        return Meteor.user().emails[0].address;
-      },
-      internalId: function(){
-        return Meteor.user()._id;
-      }
+        email: function() {
+            return Meteor.user().emails[0].address;
+        },
+        internalId: function() {
+            return Meteor.userId();
+        }
     });
-}
 
-if (Meteor.isServer) {
-    Meteor.startup(function() {
-        // code to run on server at startup
+    //Handles posting tweets
+    Template.tweetentry.events({
+        'submit #tweet-entry': function(event) {
+            // Don't refresh automatically
+            event.preventDefault();
+
+            var text = event.target[0].value;
+            var authorID = Meteor.userId();
+
+            // Validate and tweak tweet contents...
+            if (text == "") {
+                text = "I was too lazy to actually type something in";
+            };
+
+            // Debug output
+            console.log("Form submitted");
+            console.log("authorID: " + authorID);
+            console.log("Tweet text: " + text);
+
+            // Insert the tweet and metadata into the database collection
+            Tweets.insert({
+                authorID: authorID,
+                createdAt: new Date(),
+                text: text
+            }, function(err) {
+                if (err) {
+                    alert("Your tweet did not get saved");
+                } else {
+                    console.log("Insert succesful.")
+                };
+            });
+
+            // Empty the Tweet Entry form after submission
+            document.getElementById("tweet-entry-text").value = "";
+        }
+    });
+
+    // Handles getting the tweets from the server to display on the feed.
+    Template.tweetfeed.helpers({
+        tweets: function() {
+            // Filter by current user, sort newest-first.
+            return Tweets.find({
+                authorID: Meteor.userId()
+              }, {
+                sort: {
+                  createdAt: -1
+                }
+            })
+        }
     });
 }
